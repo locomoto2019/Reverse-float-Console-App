7a47d3200b3d13855cbb0b8fa3d53af9b6a12107
Loco Moto <locomoto2019@outlook.com>
2019-02-10 20:34:03 +0530

    created two static fuction that reverse string with float value.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse1("1234.56.78"));
+            Console.WriteLine(Reverse2("1234.56.78"));
+            Console.ReadLine();
+        }
+
+        static string Reverse2(string str)
+        {
+            int strLen = str.Length, elem = strLen - 1;
+            char[] charA = new char[strLen];
+
+            for (int i = 0; i < strLen; i++)
+            {
+                charA[elem] = str[i];
+                elem--;
+            }
+
+            return new string(charA);
+        }
+
+        static string Reverse1(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            for (int i = 0; i < charArray.Length / 2; i++)
+            {
+                int tmp = charArray[i];
+                charArray[i] = charArray[charArray.Length - i - 1];
+                charArray[charArray.Length - i - 1] = (char)tmp;
+            }
+            return new string(charArray);
+        }
+    }
+}
